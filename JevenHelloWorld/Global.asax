9566217bb382cbbd0f35fca712335d65216756e3
Jeven.wu <Shining.Wu@software.dell.com>
2015-09-21 14:26:43 +0800

    init project

---
+++ JevenHelloWorld/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JevenHelloWorld.MvcApplication" Language="C#" %>
---
+++ JevenHelloWorld/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JevenHelloWorld.Startup))]
+namespace JevenHelloWorld
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
